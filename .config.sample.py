168d56bed3e8c60958816e46457a499922ac6640
NANDUACHU <36236570+NANDUACHU@users.noreply.github.com>
2018-07-26 17:34:50 +0530

    Create .config.sample.py

---
+++ .config.sample.py
@@ -0,0 +1,18 @@
+class Config(object):
+    # get a token from https://chatbase.com
+    CHAT_BASE_TOKEN = ""
+    # get a token from @BotFather
+    TG_BOT_TOKEN = ""
+    # your domain to show when download file is greater than MAX_FILE_SIZE
+    HTTP_DOMAIN = "https://example.com/"
+    # the download location, where the HTTP Server runs
+    DOWNLOAD_LOCATION = "./DOWNLOADS"
+    # Telegram maximum file upload size
+    MAX_FILE_SIZE = 50000000
+    TG_MAX_FILE_SIZE = 14000000000
+    # The Telegram API things
+    APP_ID = 12345
+    API_HASH = ""
+    # Get these values from my.telegram.org
+    # for storing the Telethon session
+    TL_SESSION = "uploaderthepostmaster.session"
